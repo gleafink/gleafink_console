6a0dcbf1104b0244110b5ee4c9ac94494a535e45
Tushar Rajput <tushar.rajput@gleafink.com>
2024-07-29 10:26:44 +0530

    logo and style updated

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { ScanEye } from "lucide-react"
+import Image from "next/image"
 
 const useDebounce = (value: any, delay: any) => {
   const [debouncedValue, setDebouncedValue] = useState(value)
@@ -63,15 +64,22 @@ const HomePage = () => {
   }, [debouncedSearchQuery])
   return (
     <div className="flex justify-center items-center flex-col ">
-      <div className="flex items-center justify-between w-[100%] px-12 h-14 border-b-2  fixed top-0 z-10">
-        <div>Gleafink</div>
+      <div className="flex items-center justify-between w-[100%] px-12 h-16 border-b-2  fixed top-0 z-10 bg-white">
+        <div>
+          <Image
+            src="/images/GleafinkLogo.png"
+            alt="text"
+            width={110}
+            height={60}
+          />
+        </div>
         <div>Gleafink Console</div>
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </div>
-      <div className=" flex flex-col items-end w-3/4 py-4 mt-14">
+      <div className=" flex flex-col items-end w-3/4 py-4 mt-16">
         <div className="w-60">
           <Input
             placeholder="Filter Services"
@@ -85,22 +93,25 @@ const HomePage = () => {
           <div className="flex flex-col items-start justify-center  rounded-md p-3 mt-3  w-[100%]">
             {filteredCategories.map((item) => (
               <div key={item.name} className="w-[100%] mb-4">
-                <div className="flex justify-center items-center">
-                  <p className="text-nowrap pr-2">{item.name}</p>
-                  <div className="w-[100%] bg-black h-[1px] border-black"></div>
+                <div className="flex justify-center items-center gap-4">
+                  <h4 className="text-nowrap pr-2 text-md font-semibold">
+                    {item.name}
+                  </h4>
+                  <div className="w-[100%] bg-black h-[0.5px] border-black"></div>
                 </div>
-                <div className="flex gap-6 py-4 px-2 border-2 m-2">
+                <div className="flex gap-6 py-4   my-2">
                   {item.applications.map((item, index) => (
                     <Card
                       key={index}
-                      className="w-80  flex items-center cursor-pointer"
+                      className="w-64 flex items-center cursor-pointer border-none shadow-md"
                     >
                       <CardHeader>
                         <div className="flex gap-1">
                           {item.icon}
-                          <CardTitle className="text-lg">{item.name}</CardTitle>
+                          <CardTitle className="text-md font-light">
+                            {item.name}
+                          </CardTitle>
                         </div>
-                        {/* <CardDescription>{item.description}</CardDescription> */}
                       </CardHeader>
                     </Card>
                   ))}
